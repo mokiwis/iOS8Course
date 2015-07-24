5fdb08224dc10001dfc478010fa24e9153d91e9d
mokiwis <cervantes.moises@me.com>
2015-07-24 12:49:59 -0500

    el botón cambia el texto

--- ios8Course/ViewController.swift
+++ ios8Course/ViewController.swift
@@ -12,11 +12,15 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var miEtiqueta: UILabel!
     
+    @IBAction func botonOprimido(sender: AnyObject) {
+
+        miEtiqueta.text = "Funcionó!"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        miEtiqueta.text = "Funcionó!"
         
         println("Hola mundo!")
         
